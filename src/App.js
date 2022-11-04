08e6b2676bbf97c20312612bb0fe527941349d8f
BetterLogicTeam <102576102+BetterLogicTeam@users.noreply.github.com>
2022-11-05 00:37:38 +0500

    Final Commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Get_Data from './Components/Get_Data/Get_Data';
 import Header from './Components/Header/Header';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Uploadfile from './Components/UploadFile/Uploadfile';
 
 function App() {
   return (
     <div className="App">
       <ToastContainer/>
       <Header/>
+      {/* <Uploadfile/> */}
       <Get_Data/>
      
     </div>
--- src/Components/Get_Data/Get_Data.jsx
+++ src/Components/Get_Data/Get_Data.jsx
@@ -3,11 +3,78 @@ import { toast } from 'react-toastify';
 import { loadWeb3 } from '../apis/api';
 import { airdrop, airdrop_ABI } from '../utilies/Contract';
 import './GetData_style.css'
+import * as XLSX from 'xlsx';
+
 
 export default function Get_Data() {
     const [tokenValue, settokenValue] = useState("")
-    const [addressesValue, setaddressesValue] = useState('')
-    const [AmountsValue, setAmountsValue] = useState('')
+    const [addressesValue, setaddressesValue] = useState([])
+    const [AmountsValue, setAmountsValue] = useState([])
+    const [excelFile, setexcelFile] = useState(null)
+    const [excelFileError, setexcelFileError] = useState(null)
+    const [exclData, setexclData] = useState(null)
+    const [loader, setloader] = useState(false)
+
+
+    console.log(excelFile);
+    const fileType = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
+    const fileType2 = ['text/csv']
+    const handlefile = async (e) => {
+
+        let selectFile = e.target.files[0]
+        // console.log("Type", selectFile.type);
+        if (selectFile) {
+            if (selectFile && fileType.includes(selectFile.type)) {
+                let reader = new FileReader()
+                reader.readAsArrayBuffer(selectFile)
+                reader.onload = (e) => {
+                    setexcelFileError(null)
+                    setexcelFile(e.target.result)
+                }
+
+
+            } else {
+                setexcelFileError("Please Select Only Excel File")
+                setexcelFile(null)
+
+            }
+
+
+        } else {
+
+        }
+    }
+
+    const handleSubmit = async () => {
+
+        if (excelFile != null) {
+            const workbook = XLSX.read(excelFile, { type: 'buffer' })
+            const workSheetName = workbook.SheetNames[0];
+            const workSheet = workbook.Sheets[workSheetName]
+            const data = XLSX.utils.sheet_to_json(workSheet)
+            // console.log("DATA", data);
+            let AddresArray = []
+            let AmountArray = []
+            const web3 = window.web3;
+
+            data.forEach(items => {
+                // console.log("Address_Towei",items.Amounts);
+                let AddressData = web3.utils.toWei((items.Amounts).toString())
+                AddresArray = [...AddresArray, items.Address]
+                AmountArray = [...AmountArray, AddressData]
+
+            });
+            setaddressesValue(AddresArray)
+            setAmountsValue(AmountArray)
+            setexclData(data)
+
+        } else {
+            setexclData(null)
+            setaddressesValue([])
+            setAmountsValue([])
+        }
+
+    }
 
 
     const Submit_data = async () => {
@@ -20,33 +87,70 @@ export default function Get_Data() {
         } else {
 
             try {
-                
-                let pathArray = addressesValue.split(',');
-                let Number_Array=AmountsValue.split(',')
-                console.log("pathArray",pathArray.length);
+                setloader(true)
+                // console.log("pathArray", addressesValue, AmountsValue);
 
-                if(Number_Array.length==pathArray.length){
+                // let pathArray = addressesValue.split(',');
+                // let Number_Array = AmountsValue.split(',')
+
+                if (addressesValue.length == AmountsValue.length) {
 
                     const web3 = window.web3;
                     let contractOf = new web3.eth.Contract(airdrop_ABI, airdrop);
-                    await contractOf.methods.multisendToken(tokenValue,pathArray,Number_Array).send({
+                    await contractOf.methods.multisendToken(tokenValue, addressesValue, AmountsValue).send({
                         from: acc
                     });
                     toast.success('Transition Confirm')
+                    setloader(false)
 
-                }else{
+
+                } else {
                     toast.error("Array length is not match")
-                    
+                    setloader(false)
+
+
                 }
 
             } catch (e) {
                 console.log("Error While data", e);
+                setloader(false)
+
             }
         }
     }
 
     return (
         <div>
+
+
+
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-lg-2">
+
+                    </div>
+
+                    <div className="col-lg-8">
+
+                        <div className="card claim_card">
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Upload File</label>
+                                <input type="file" class="form-control" onChange={handlefile} />
+                                <label htmlFor="" className='text-danger mt-1'>{excelFileError}</label>
+                            </div>
+
+                            <div class="col-auto">
+                                <button className='claim_btn' onClick={() => handleSubmit()} >Upload File</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-lg-2">
+
+                    </div>
+                </div>
+            </div>
+
             <div className="container mt-5">
                 <div className="row">
                     <div className="col-lg-2">
@@ -58,18 +162,28 @@ export default function Get_Data() {
                         <div className="card claim_card">
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" class="form-label">Token address</label>
-                                <input type="text" class="form-control"  placeholder="Token address" onChange={(e)=>settokenValue(e.target.value)} />
+                                <input type="text" class="form-control" placeholder="Token address" onChange={(e) => settokenValue(e.target.value)} />
                             </div>
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" class="form-label">Addresses</label>
-                                <input type="text" class="form-control"  placeholder="Addresses Arrays" onChange={(e)=>setaddressesValue(e.target.value)} />
+                                <input type="text" class="form-control" value={addressesValue} placeholder="Addresses Arrays" onChange={(e) => setaddressesValue(e.target.value)} />
                             </div>
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" class="form-label">Amounts</label>
-                                <input type="text" class="form-control"  placeholder="Amounts Arrays" onChange={(e)=>setAmountsValue(e.target.value)} />
+                                <input type="text" class="form-control" value={AmountsValue} placeholder="Amounts Arrays" onChange={(e) => setAmountsValue(e.target.value)} />
                             </div>
                             <div class="col-auto">
-                                <button className='claim_btn' onClick={() => Submit_data()}>Send</button>
+                                <button className='claim_btn' onClick={() => Submit_data()}>
+                                    {
+                                        loader ? <>
+                                            <div class="spinner-border" role="status">
+                                                <span class="visually-hidden">Loading...</span>
+                                            </div>
+                                        </>
+                                            : "Send"
+
+                                    }
+                                </button>
                             </div>
                         </div>
 
